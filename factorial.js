07159afa91c49113e7ccc7239bae0eac9f38254c
vallapusathvika <vallapusathvika22@gmail.com>
2026-05-04 14:45:50 +0530

    initial commit

---
+++ factorial.js
@@ -0,0 +1,18 @@
+let num=5
+fact=1
+if (num<0){
+    console.log("invalid");
+}
+else if (num==0){
+    console.log("factorial is 1");
+}
+else{
+    for(let i=1;i<num;i++){
+        fact=fact*i;
+    console.log(fact);
+    }
+    
+    }
+
+
+    
