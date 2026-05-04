26c52d39431f7935e49f19d7bcf79f011c9e92ff
vallapusathvika <vallapusathvika22@gmail.com>
2026-05-04 20:09:19 +0530

    initial commit

---
+++ countvowels.js
@@ -0,0 +1,15 @@
+str="full stack"
+const vowels=function(str){
+    let count=0;
+    for(let i=0;i<str.length;i++){
+        let ch=str[i];
+    
+    if(ch==='a'|| ch==="e" ||ch==="i"||ch==="o" ||ch==="u" ||ch==="A" ||ch==="E"||ch==="I"||ch==="O"||ch==="U"){
+        count++;
+    }
+
+    }
+    return count;
+
+}
+console.log(vowels(str))
