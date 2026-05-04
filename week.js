071f41e6bfa0307a769fe8208e9c923bf4d8e248
vallapusathvika <vallapusathvika22@gmail.com>
2026-05-04 14:31:21 +0530

    initial commi

---
+++ week.js
@@ -0,0 +1,16 @@
+let day=2
+console.log(day)
+switch (day){
+    case 1:
+        console.log("mon")
+        break
+    case  2:
+        console.log("tue")
+        break
+    case 3:
+        console.log("wed")
+        break
+    default:
+        console.log("invalid")
+
+}
